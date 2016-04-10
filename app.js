9fc7c661463e453b2d0847729b6a06fa272627a7
seraphion <djurdjevicster@gmail.com>
2016-04-10 23:05:49 +0200

    Add like animation and jQuery

---
+++ app.js
@@ -0,0 +1,12 @@
+//-----------------------------------------------
+//  Document ready
+//-----------------------------------------------
+
+$(document).ready(function() {
+  $('.js-like').on('click', function(event) {
+    event.preventDefault();
+    $(this).text('Liked!')
+      .closest('.news-item')
+      .addClass('is-liked');
+  });
+});
