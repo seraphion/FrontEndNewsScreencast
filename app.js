555f66f47eeb560ab79a6774013b1563361821fe
seraphion <djurdjevicster@gmail.com>
2016-04-11 19:26:35 +0200

    Add modal functionality

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 //-----------------------------------------------
 
 $(document).ready(function() {
+
+  //-----------------------------------------------
+  //  Like
+  //-----------------------------------------------
+
   $('.js-like').on('click', function(event) {
     event.preventDefault();
     $(this).text('Liked!')
@@ -10,8 +15,27 @@ $(document).ready(function() {
       .addClass('is-liked');
   });
 
+  //-----------------------------------------------
+  //  Add link
+  //-----------------------------------------------
+
   $('.js-add-link').on('click', function(event) {
     event.preventDefault();
     $('.js-form').toggleClass('is-visible');
+  });
+
+  //-----------------------------------------------
+  //  Modal
+  //-----------------------------------------------
+
+  $('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
   });
 });
